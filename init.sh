b762c6230ebaae8888b8f33fc8a0ab906415cdc4
William Agudelo <wagudelos@gmail.com>
2021-03-30 13:34:49 -0500

    Modificacion del init.sh

--- init.sh
+++ init.sh
@@ -2,9 +2,10 @@
 
 #*************************************
 # The MCH Binding Interaction (MHCBI) pipeline
-# @author Carlos Andres Ortiz-Mahecha
-#  (email: caraortizmah@gmail.com)
-#  (email: caraortizmah@unal.edu.co)
+# Author: FIDIC Biomathematics Group
+# Citation: Ortiz-Mahecha CA, Agudelo WA, Patarroyo MA, Patarroyo ME and Suarez CF. MHCBI: a pipeline for calculating peptide-protein binding energy using     semi-empirical quantum mechanical methods with explicit/implicit solvent models. submitted
+# Contact: fidic.biomath@gmail.com, caraortizmah@gmail.com
+#***************************************
 # @comment:
 #  This is the initial program enabling installation, setup, and running.
 #*************************************
@@ -14,19 +15,16 @@
 #******                                                                            ******
 #******           This pipeline has been developed by                              ******
 #******                                                                            ******
-#******               CARLOS ANDRES ORTIZ-MAHECHA                                  ******
-#******                 caraortizmah@gmail.com                                     ******
-#******                caraortizmah@unal.edu.co                                    ******
+#******               FIDIC Biomathematics Group                                   ******
 #******                                                                            ******
-#****** Further info :                                                             ******
-#******                 https://github.com/caraortizmah/BindingInteraction         ******
+#****** Further info:  https://github.com/fidic-biomath/BindingInteraction         ******
 #****************************************************************************************
 
 echo "**************************************************************"
 echo "|||               Welcome to The MHCBI pipeline            |||"
 echo "|||                                                        |||"
 echo "||| Further info:                                          |||"
-echo "|||   https://github.com/caraortizmah/BindingInteraction   |||"
+echo "|||   https://github.com/fidic-biomath/BindingInteraction  |||"
 echo "|||                                                        |||"
 echo "**************************************************************"
 
